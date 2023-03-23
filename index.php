7275352cdbf1ee5208849feb874fa9053463cdb4
Brian Atkinson <76421912+BrianAtkinson93@users.noreply.github.com>
2023-03-22 23:06:55 -0700

    adding more functions for modularity

--- index.php
+++ index.php
@@ -1,57 +1,12 @@
-<!DOCTYPE html>
 <?php
+include 'includes/header.php';
+require_once 'includes/config.php';
 require_once 'includes/functions.php';
-session_start();
 ?>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Online Crime Reporting System</title>
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="assets/css/styles.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css">
-</head>
+
 <body>
 <!-- Hamburger Menu -->
-<!-- Hamburger Menu -->
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">OCRS</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-            <li class="nav-item active">
-                <a class="nav-link" href="index.php">Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="view/lost_found.php">Lost & Found</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="view/missing_persons.php">Missing Persons</a>
-            </li>
-            <!-- Add more menu items as needed -->
-        </ul>
-        <ul class="navbar-nav ml-auto">
-            <?php if (!isset($_SESSION['email'])): ?>
-                <li class="nav-item">
-                    <a class="nav-link" href="login.php">Login</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="register.php">Register</a>
-                </li>
-            <?php else: ?>
-                <li class="nav-item">
-                    <a class="nav-link" href="logout.php">Logout</a>
-                </li>
-            <?php endif; ?>
-        </ul>
-    </div>
-</nav>
+<?php include 'includes/menu.php'; ?>
 
 <!-- Most Wanted Section -->
 <section class="container mt-5">
@@ -117,17 +72,4 @@ session_start();
     </div>
 </section>
 
-<!-- Footer -->
-<footer class="bg-light py-3 mt-5">
-    <div class="container">
-        <p class="text-center mb-0">Online Crime Reporting System &copy; 2023</p>
-    </div>
-</footer>
-
-<!-- Optional JavaScript -->
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-</body>
-</html>
+<?php include 'includes/footer.php'; ?>
--- view/missing_persons.php
+++ view/missing_persons.php
@@ -1,22 +1,37 @@
 <?php
 require_once '../includes/functions.php';
-$missing_persons = getAllMissingPersons(); // Implement this function in functions.php
+$missing_persons = getAllReports('missing_persons');
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>View Missing Persons</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css">
 </head>
 <body>
-<h1>Reported Missing Persons</h1>
-<!-- Display the list of reported missing persons -->
-<?php while ($row = $missing_persons->fetch_assoc()): ?>
-    <!-- Customize the display of missing person details -->
-    <div>
-        <h2><?php echo $row['name']; ?></h2>
-        <p><?php echo $row['description']; ?></p>
+<section class="container mt-5">
+    <h1>Reported Missing Persons</h1>
+    <div class="row">
+        <?php while ($row = $missing_persons->fetch_assoc()): ?>
+            <!-- Customize the display of missing person details -->
+            <div class="col-md-4">
+                <h3><?php echo $row['name']; ?></h3>
+                <p><?php echo $row['description']; ?></p>
+            </div>
+        <?php endwhile; ?>
     </div>
-<?php endwhile; ?>
+</section>
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
