d88e188b00690ea2ffd15b805d5bafb3d37f454c
Brian Atkinson <76421912+BrianAtkinson93@users.noreply.github.com>
2023-03-22 18:28:44 -0700

    adding basics

--- admin/mange_reports.php
+++ admin/mange_reports.php
@@ -1,1 +1,29 @@
 <?php
+require_once '../includes/functions.php';
+
+if (!isAuthenticated()) {
+    header("Location: ../login.php");
+    exit();
+}
+
+$reports = getAllReports(); // Implement this function in functions.php
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Manage Reports</title>
+</head>
+<body>
+<h1>Manage Reports</h1>
+<!-- Display the list of reports -->
+<?php while ($row = $reports->fetch_assoc()): ?>
+    <!-- Customize the display of report details -->
+    <div>
+        <h2><?php echo $row['report_type']; ?></h2>
+        <p><?php echo $row['description']; ?></p>
+    </div>
+<?php endwhile; ?>
+<!-- Add functionality to edit, delete, or mark reports as resolved -->
+</body>
+</html>
